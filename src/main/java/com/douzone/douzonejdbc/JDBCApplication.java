9baba2e674491cd0dca57065e1bdb09a96f25466
d0uwhs <d0uwhs@gmail.com>
2022-08-31 19:38:30 +0900

    Init Commit.

---
+++ src/main/java/com/douzone/douzonejdbc/JDBCApplication.java
@@ -0,0 +1,7 @@
+package com.douzone.douzonejdbc;
+
+public class JDBCApplication {
+    public static void main(String[] args) {
+        System.out.println("JDBCApplication");
+    }
+}
---
+++ src/main/java/com/douzone/douzonejdbc/dto/ProductDto.java
@@ -0,0 +1,10 @@
+package com.douzone.douzonejdbc.dto;
+
+public class ProductDto {
+    private String ioNum;
+    private String productId;
+    private String pName;
+    private String ioDate;
+    private String amount;
+    private String status;
+}
